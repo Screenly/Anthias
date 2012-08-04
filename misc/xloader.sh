2e646b5feecea663e5b5667ccce7f6441aa19b0a
Viktor Petersson <vpetersson@wireload.net>
2012-08-04 16:10:29 +0200

    Added logging to xloader.sh

--- misc/xloader.sh
+++ misc/xloader.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+LOG=/tmp/screenly_xloader.log
+
+echo "Disabling screen power savings..." > $LOG
+
 xset s off         # don't activate screensaver
 xset -dpms         # disable DPMS (Energy Star) features.
 xset s noblank     # don't blank the video device
 
 sleep 5
+
+echo "Launching infinite loop..." >> $LOG
 while true
 do
-    # Clean up in case of an unclean exit
-    killall uzbl-core
-    rm /tmp/uzbl_*
-    rm /tmp/screenly_html/*
+	# Clean up in case of an unclean exit
+	echo "Cleaning up..." >> $LOG
+	killall uzbl-core
+	rm /tmp/uzbl_*
+	rm /tmp/screenly_html/*
     
-    # Launch the viewer
-    python ~/screenly/viewer.py
+	# Launch the viewer
+	python ~/screenly/viewer.py >> $LOG 2>&1
 done
