Screenly/Anthias

commit df1e94688db4c19d6dd4f22719c343be38c6dd69

Dropped database column (make sure to run migration.py), otherwise Screenly will break.
Added more more redundancy to web_view by first checking for a 200-status before trying to load with the browser.

viewer.py
@@ -44,13 +44,12 @@ def generate_asset_list():
     for asset in query:
         asset_id = asset[0]  
         name = asset[1]
-        filename = asset[2]
-        uri = asset[3]
-        md5 = asset[4]
-        start_date = asset[5]
-        end_date = asset[6]
-        duration = asset[7]
-        mimetype = asset[8]
+        uri = asset[2]
+        md5 = asset[3]
+        start_date = asset[4]
+        end_date = asset[5]
+        duration = asset[6]
+        mimetype = asset[7]
 
         if (start_date and end_date) and (start_date < time_lookup() and end_date > time_lookup()):
             playlist.append({"name" : name, "uri" : uri, "duration" : duration, "mimetype" : mimetype})
@@ -125,16 +124,24 @@ def view_video(video):
             logging.debug("Unclean exit: " + str(run))
 
 def view_web(url, duration):
-    logging.debug('Displaying url %s for %s seconds.' % (url, duration))
-    f = open(fifo, 'a')
-    f.write('set uri = %s\n' % url)
-    f.close()
+
+    # Make sure the resource is available before trying loading it.
+    web_resource = requests.get(url).status_code
+    if web_resource == 200:
+        logging.debug('Web content appears to be available. Proceeding.')  
+        logging.debug('Displaying url %s for %s seconds.' % (url, duration))
+        f = open(fifo, 'a')
+        f.write('set uri = %s\n' % url)
+        f.close()
     
-    sleep(int(duration))
+        sleep(int(duration))
     
-    f = open(fifo, 'a')
-    f.write('set uri = %s\n' % black_page)
-    f.close()
+        f = open(fifo, 'a')
+        f.write('set uri = %s\n' % black_page)
+        f.close()
+    else: 
+        logging.debug('Received non-200 status from %s. Skipping.' % (url))
+        pass
 
 logging.debug('Starting viewer.py')
 

misc/migrate.py
@@ -1,0 +1,39 @@
+import sqlite3, os
+
+# Define settings
+configdir = os.path.join(os.getenv('HOME'), '.screenly/')
+database = os.path.join(configdir, 'screenly.db')
+
+try: 
+    conn = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
+    c = conn.cursor()
+    c.execute("SELECT filename FROM assets")
+    filename_exist = c.fetchone()
+    c.close()
+except:
+    filename_exist = False
+
+# if the column 'filename' exist, drop it
+if filename_exist:
+    conn = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
+    c = conn.cursor()
+
+    migration= """
+        BEGIN TRANSACTION;
+        CREATE TEMPORARY TABLE assets_backup(asset_id, name, uri, md5, start_date, end_date, duration, mimetype);
+        INSERT INTO assets_backup SELECT asset_id, name, uri, md5, start_date, end_date, duration, mimetype FROM assets;
+        DROP TABLE assets;
+        CREATE TABLE assets(asset_id TEXT, name TEXT, uri TEXT, md5 TEXT, start_date TIMESTAMP, end_date TIMESTAMP, duration TEXT, mimetype TEXT);
+        INSERT INTO assets SELECT asset_id, name, uri, md5, start_date, end_date, duration, mimetype FROM assets_backup;
+        DROP TABLE assets_backup;
+        COMMIT;
+        """
+
+    c.executescript(migration)
+    c.close()
+    print "Dropped obsolete column (filename)"
+
+else:
+    pass
+
+print "Migration done."

server.py
@@ -4,7 +4,7 @@
 __author__ = "Viktor Petersson"
 __copyright__ = "Copyright 2012, WireLoad Inc"
 __license__ = "Dual License: GPLv2 and Commercial License"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __email__ = "vpetersson@wireload.net"
 
 import json, hashlib, os, requests, mimetypes, sys, sqlite3, socket, netifaces
@@ -39,18 +39,17 @@ def get_playlist():
         # Match variables with database
         asset_id = asset[0]  
         name = asset[1]
-        filename = asset[2]
-        uri = asset[3] # Path in local database
-	input_start_date = asset[5]
-	input_end_date = asset[6]
+        uri = asset[2] # Path in local database
+	    input_start_date = asset[4]
+	    input_end_date = asset[5]
 
         try:
-            start_date = datestring.date_to_string(asset[5])
+            start_date = datestring.date_to_string(asset[4])
         except:
             start_date = None
 
         try:
-            end_date = datestring.date_to_string(asset[6])
+            end_date = datestring.date_to_string(asset[5])
         except:
             end_date = None
             
@@ -83,16 +82,15 @@ def get_assets():
         # Match variables with database
         asset_id = asset[0]  
         name = asset[1]
-        filename = asset[2]
-        uri = asset[3] # Path in local database
+        uri = asset[2] # Path in local database
 
         try:
-            start_date = datestring.date_to_string(asset[5])
+            start_date = datestring.date_to_string(asset[4])
         except:
             start_date = ""
 
         try:
-            end_date = datestring.date_to_string(asset[6])
+            end_date = datestring.date_to_string(asset[5])
         except:
             end_date = ""
             
@@ -128,7 +126,7 @@ def initiate_db():
     asset_table = c.fetchone()
     
     if not asset_table:
-        c.execute("CREATE TABLE assets (asset_id TEXT, name TEXT, filename TEXT, uri TEXT, md5 TEXT, start_date TIMESTAMP, end_date TIMESTAMP, duration TEXT, mimetype TEXT)")
+        c.execute("CREATE TABLE assets (asset_id TEXT, name TEXT, uri TEXT, md5 TEXT, start_date TIMESTAMP, end_date TIMESTAMP, duration TEXT, mimetype TEXT)")
         return "Initiated database."
     
 @route('/process_asset', method='POST')
@@ -169,12 +167,11 @@ def process_asset():
             if "video" in mimetype:
                 duration = "N/A"
 
-            filename = uri_check.path.split('/')[-1]
             start_date = ""
             end_date = ""
             duration = ""
             
-            c.execute("INSERT INTO assets (asset_id, name, filename, uri, start_date, end_date, duration, mimetype) VALUES (?,?,?,?,?,?,?,?)", (asset_id, name, filename, uri, start_date, end_date, duration, mimetype))
+            c.execute("INSERT INTO assets (asset_id, name, uri, start_date, end_date, duration, mimetype) VALUES (?,?,?,?,?,?,?,?)", (asset_id, name, uri, start_date, end_date, duration, mimetype))
             conn.commit()
             
             header = "Yay!"
@@ -372,22 +369,21 @@ def edit_asset(asset_id):
     
     asset_id = asset[0]
     name = asset[1]
-    filename = asset[2]
-    uri = asset[3]
-    md5 = asset[4]
+    uri = asset[2]
+    md5 = asset[3]
 
-    if asset[5]:
+    if asset[4]:
 	    start_date = datestring.date_to_string(asset[5])
     else:
 	    start_date = None
 
-    if asset[6]:
+    if asset[5]:
 	    end_date = datestring.date_to_string(asset[6])
     else:
 	    end_date = None
 
-    duration = asset[7]
-    mimetype = asset[8]
+    duration = asset[6]
+    mimetype = asset[7]
 
     assetdict = {
             "name" : name,

commit cf33886b0e5cc74a7b3d2011a1b291769dc4aaae

Fixed fatal bug that was introduced in commit df1e94688db4c19d6dd4f22719c343be38c6dd69.

viewer.py
@@ -125,8 +125,16 @@ def view_video(video):
 
 def view_web(url, duration):
 
-    # Make sure the resource is available before trying loading it.
-    web_resource = requests.get(url).status_code
+    # If local web page, check if the file exist. If remote, check if it is
+    # available.
+    if html_folder in url:
+        if os.path.exists(url):
+            web_resource = 200
+        else:
+            breal
+    else:
+        web_resource = requests.get(url).status_code
+
     if web_resource == 200:
         logging.debug('Web content appears to be available. Proceeding.')  
         logging.debug('Displaying url %s for %s seconds.' % (url, duration))
@@ -140,7 +148,7 @@ def view_web(url, duration):
         f.write('set uri = %s\n' % black_page)
         f.close()
     else: 
-        logging.debug('Received non-200 status from %s. Skipping.' % (url))
+        logging.debug('Received non-200 status (or file not found if local) from %s. Skipping.' % (url))
         pass
 
 logging.debug('Starting viewer.py')