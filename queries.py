1e0fc31f89161faad8ee45f1a0e6622fbfd3c425
Marton Somogyi <mcbuddha@gmail.com>
2013-02-27 14:38:00 +0100

    Set asset_id as primary key in database. refs #84

--- queries.py
+++ queries.py
@@ -2,7 +2,7 @@ exists_assets_table = """
 SELECT name FROM sqlite_master WHERE type='table' AND name='assets'
 """
 create_assets_table = """
-CREATE TABLE assets (asset_id TEXT, name TEXT, uri TEXT, md5 TEXT, start_date TIMESTAMP, end_date TIMESTAMP, duration TEXT, mimetype TEXT, is_enabled INTEGER default 0, nocache INTEGER default 0)
+CREATE TABLE assets (asset_id TEXT PRIMARY KEY, name TEXT, uri TEXT, md5 TEXT, start_date TIMESTAMP, end_date TIMESTAMP, duration TEXT, mimetype TEXT, is_enabled INTEGER default 0, nocache INTEGER default 0)
 """
 
 comma = lambda l: ','.join(l)
