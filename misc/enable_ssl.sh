6875179d59ad18dc9d4111f6bb69ab85a6ffb565
Viktor Petersson <vpetersson@wireload.net>
2014-10-08 15:12:21 +0200

    Closes #225. Adds note about firewall.

--- misc/enable_ssl.sh
+++ misc/enable_ssl.sh
@@ -27,3 +27,7 @@ echo "Starting Stunnel..."
 sudo /etc/init.d/stunnel4 restart
 
 echo "You should be all set. You should be able to access Screenly's management interface at https://<your IP>"
+
+echo "NOTE: If you have the firewall enabled, make sure to open it up for HTTPS (port 443)."
+echo "Assuming you're running UFW, the command is:"
+echo "sudo ufw allow 443/tcp"
