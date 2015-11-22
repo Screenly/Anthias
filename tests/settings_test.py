399a7d792a50d3ed628ff78518fe953614ec8289
over <overln2@gmail.com>
2015-11-22 20:53:30 +0600

    fix code style, typos in settings_test

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -3,10 +3,24 @@ from settings import DEFAULTS
 from settings import config_dir
 from copy import deepcopy
 from os import getenv
+import os
 from settings import settings as s
+
+SETTINGS_LOAD_FROM = '/tmp/settings'
+SETTINGS_SAVE_TO = '/tmp/settings.new'
 
 
 class SettingsTest(unittest.TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove(SETTINGS_LOAD_FROM)
+        except: pass
+
+        try:
+            os.remove(SETTINGS_SAVE_TO)
+        except: pass
+
     def test_config_dir_should_be_HOME_with_path(self):
         home = getenv('HOME')
         self.assertEquals(config_dir(home='/tmp'), '/tmp/.screenly/')
@@ -48,14 +62,14 @@ class SettingsTest(unittest.TestCase):
         s['listen'] = '127.0.0.1:8080'
         self.assertEqual(s['listen'], '127.0.0.1:8080')
 
-    def test_load_non_exists_file_should_rise_io_error(self):
+    def test_load_non_exists_file_should_raise_io_error(self):
         self.assertRaises(IOError, lambda: s.load('/tmp/notfound'))
 
     def load_with_conf(self, conf_str):
-        file = '/tmp/settings'
-        with open(file, 'w') as f:
+        with open(SETTINGS_LOAD_FROM, 'w') as f:
             f.write(conf_str)
-        s.load(file)
+        s.load(SETTINGS_LOAD_FROM)
+
 
     def conf_equal(self, file, lines):
         with open(file) as f:
@@ -75,21 +89,22 @@ class SettingsTest(unittest.TestCase):
         self.load_with_conf('[main]\nassetdir = dir')
         s['show_splash'] = False
 
-        save_to = '/tmp/settings.new'
-        s.save(save_to)
+        s.save(SETTINGS_SAVE_TO)
 
-        self.conf_equal(save_to, ['[main]\n',
-                                  'assetdir = dir\n',
-                                  'database = .screenly/screenly.db\n',
-                                  'listen = 0.0.0.0:8080\n',
-                                  'use_24_hour_clock = False\n',
-                                  '\n',
-                                  '[viewer]\n',
-                                  'audio_output = hdmi\n',
-                                  'debug_logging = False\n',
-                                  'default_duration = 10\n',
-                                  'resolution = 1920x1080\n',
-                                  'show_splash = False\n',
-                                  'shuffle_playlist = False\n',
-                                  'verify_ssl = True\n',
-                                  '\n'])
+        self.conf_equal(SETTINGS_SAVE_TO, [
+            '[main]\n',
+            'assetdir = dir\n',
+            'database = .screenly/screenly.db\n',
+            'listen = 0.0.0.0:8080\n',
+            'use_24_hour_clock = False\n',
+            '\n',
+            '[viewer]\n',
+            'audio_output = hdmi\n',
+            'debug_logging = False\n',
+            'default_duration = 10\n',
+            'resolution = 1920x1080\n',
+            'show_splash = False\n',
+            'shuffle_playlist = False\n',
+            'verify_ssl = True\n',
+            '\n'
+        ])
