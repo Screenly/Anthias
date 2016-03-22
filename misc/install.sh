c1a9f978f448e03151c94af3faf7b799217da2b7
over <overln2@gmail.com>
2016-03-22 17:27:36 +0600

    fix repo path

--- misc/install.sh
+++ misc/install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y python-dev python-pip git-core
 sudo pip install ansible==2.0.1.0
 
-ansible localhost -m git -a "repo=git://github.com/over64/screenly-ose.git dest=/home/pi/screenly"
+ansible localhost -m git -a "repo=git://github.com/wireload/screenly-ose.git dest=/home/pi/screenly"
 cd /home/pi/screenly/misc/ansible
 ansible-playbook system.yml
 ansible-playbook screenly.yml
