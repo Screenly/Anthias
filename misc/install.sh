09e01576abefa90aa918c34192ae216540cf214f
over <overln2@gmail.com>
2016-03-22 16:39:00 +0600

    minor fixes

--- misc/install.sh
+++ misc/install.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+set -xe
 sudo apt-get update
-sudo apt-get install -y python-setuptools python-dev git-core
-sudo easy_install pip
-sudo pip install ansible
+sudo apt-get install -y python-dev python-pip git-core
+sudo pip install ansible==2.0.1.0
 
-ansible localhost -m git -a "repo=git://github.com/over64/screenly-ose.git dest=/home/pi/screenly version=master" || exit
+ansible localhost -m git -a "repo=git://github.com/over64/screenly-ose.git dest=/home/pi/screenly"
 cd /home/pi/screenly/misc/ansible
-ansible-playbook system.yml || exit
-ansible-playbook screenly.yml || exit
+ansible-playbook system.yml
+ansible-playbook screenly.yml
