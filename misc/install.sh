87742c3e24099a81ba5d1540b53c92131c5ac32a
over <overln2@gmail.com>
2016-04-07 17:02:39 +0600

    Parametrize install.sh with remote repo and branch. By default wireload/screenly-ose ~ master

--- misc/install.sh
+++ misc/install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y python-dev python-pip git-core
 sudo pip install ansible==2.0.1.0
 
-ansible localhost -m git -a "repo=git://github.com/wireload/screenly-ose.git dest=/home/pi/screenly"
+ansible localhost -m git -a "repo=${1:-git://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=${2:-master}"
 cd /home/pi/screenly/misc/ansible
 ansible-playbook system.yml
 ansible-playbook screenly.yml
