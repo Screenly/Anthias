815fc11439c682f27da484f0122851e37ca4eada
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 15:36:24 +0100

    Fixes output.

--- bin/enable_ssl.sh
+++ bin/enable_ssl.sh
@@ -3,6 +3,7 @@
 cd ~/screenly/ansible
 ansible-playbook -t enable-ssl site.yml
 
+set +x
 echo "You should be all set. You should be able to access Screenly's management interface at https://<your IP>"
 
 echo "NOTE: If you have the firewall enabled, make sure to open it up for HTTPS (port 443)."
