0082a6ebae491729ef4cb21bed25114f8446bcde
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 15:32:24 +0100

    Switches out stunnel with nginx.

--- bin/install.sh
+++ bin/install.sh
@@ -8,9 +8,9 @@ fi
 
 echo && read -p "Would you like to perform a full system upgrade as well? (y/N)" -n 1 -r -s && echo
 if ! [[ $REPLY =~ ^[Yy]$  ]]; then
-  EXTRA_ARGS="--skip-tags stunnel,system-upgrade"
+  EXTRA_ARGS="--skip-tags enable-ssl,system-upgrade"
 else
-  EXTRA_ARGS="--skip-tags stunnel,"
+  EXTRA_ARGS="--skip-tags enable-ssl,stunnel,"
 fi
 
 set -x
