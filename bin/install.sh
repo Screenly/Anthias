d2d48835181f2c8f7967cc85a166026e012b973c
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 11:44:24 +0100

    Adds choice to installation script.

--- bin/install.sh
+++ bin/install.sh
@@ -7,4 +7,11 @@ sudo pip install ansible==2.0.2.0
 
 ansible localhost -m git -a "repo=${1:-http://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=${2:-master}"
 cd /home/pi/screenly/ansible
-ansible-playbook site.yml --skip-tags "system-upgrade"
+
+read -p "Would you like to perform a full system upgrade as well? (y/N)" -n 1 -r -s
+echo
+if ! [[ $REPLY =~ ^[Yy]$  ]]; then
+  ansible-playbook site.yml
+else
+  ansible-playbook site.yml --skip-tags "system-upgrade"
+fi
