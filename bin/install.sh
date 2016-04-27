2f7f9df3801e7ba323089782e3518b0177801b61
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 11:49:19 +0100

    Fixes logic in install script and some logic.

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -xe
+#!/bin/bash -xe
+
 sudo apt-get update
 sudo apt-get install -y python-dev python-setuptools git-core
 sudo easy_install pip
@@ -8,10 +8,12 @@ sudo pip install ansible==2.0.2.0
 ansible localhost -m git -a "repo=${1:-http://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=${2:-master}"
 cd /home/pi/screenly/ansible
 
-read -p "Would you like to perform a full system upgrade as well? (y/N)" -n 1 -r -s
-echo
+set +x
+read -p "Would you like to perform a full system upgrade as well? (y/N)" -n 1 -r -s && echo
 if ! [[ $REPLY =~ ^[Yy]$  ]]; then
-  ansible-playbook site.yml
-else
+  set -e
   ansible-playbook site.yml --skip-tags "system-upgrade"
+else
+  set -e
+  ansible-playbook site.yml
 fi
