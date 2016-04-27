67d68c17e6214ead0e4485b7682fcb4b8b48d85f
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 10:58:08 +0100

    Refs #385. Adds support for upgrading kernel.

--- bin/install.sh
+++ bin/install.sh
@@ -7,4 +7,4 @@ sudo pip install ansible==2.0.2.0
 
 ansible localhost -m git -a "repo=${1:-http://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=${2:-master}"
 cd /home/pi/screenly/ansible
-ansible-playbook site.yml
+ansible-playbook site.yml --skip-tags "system-upgrade"
