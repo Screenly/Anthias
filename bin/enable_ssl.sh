1d30dc7f2de58244fdfa23d0631362371f21d0a8
Viktor Petersson <vpetersson@wireload.net>
2016-04-27 15:34:15 +0100

    Fixes script.

--- bin/enable_ssl.sh
+++ bin/enable_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 cd ~/screenly/ansible
-ansible-playbook -t stunnel site.yml
+ansible-playbook -t enable-ssl site.yml
 
 echo "You should be all set. You should be able to access Screenly's management interface at https://<your IP>"
 
