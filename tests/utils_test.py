0369446330f8abe323321911738cd9e15197a04c
over <overln2@gmail.com>
2016-07-25 17:55:08 +0600

    typo fix

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import unittest
 from lib import utils
 
+
 class UtilsTest(unittest.TestCase):
     def test_unicode_correctness_in_bottle_templates(self):
         self.assertEqual(utils.template_handle_unicode('hello'), u'hello')
