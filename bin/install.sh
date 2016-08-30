96675fde153c23ac8754af797aebca541d427c04
Leonel Williams <leonelwilliams@users.noreply.github.com>
2016-08-30 10:13:10 -0700

    Use https:// for GitHub repo
    GitHub does a 301 from http:// to https:// - we can avoid the redirect and interception risk if https:// is used directly

--- bin/install.sh
+++ bin/install.sh
@@ -61,7 +61,7 @@ sudo apt-get install -y python-dev git-core libffi-dev libssl-dev
 curl -s https://bootstrap.pypa.io/get-pip.py | sudo python
 sudo pip install ansible==2.1.0.0
 
-ansible localhost -m git -a "repo=${1:-http://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=$BRANCH"
+ansible localhost -m git -a "repo=${1:-https://github.com/wireload/screenly-ose.git} dest=/home/pi/screenly version=$BRANCH"
 cd /home/pi/screenly/ansible
 
 ansible-playbook site.yml $EXTRA_ARGS
