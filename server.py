e552a9f57d9e9c5ff44a0a3f31ffc0b429801c6b
TonyLHansen <TonyLHansen@users.noreply.github.com>
2016-10-12 23:55:11 -0400

    remove pep8 stylistic nits

--- server.py
+++ server.py
@@ -105,6 +105,7 @@ def template(template_name, **context):
 def check(user, passwd):
     return settings.check_user(user, passwd)
 
+
 ################################
 # API
 ################################
