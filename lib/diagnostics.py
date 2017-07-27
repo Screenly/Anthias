25ffee4ab9927392152529ec41ac5180400c05b7
daniep01 <git@pjdaniel.org.uk>
2017-07-27 09:50:47 +0100

    add display_status function and update report

--- lib/diagnostics.py
+++ lib/diagnostics.py
@@ -59,6 +59,15 @@ def get_monitor_status():
     except:
         return 'Unable to run tvservice.'
 
+def get_display_status():
+    try:
+        display_status = sh.vcgencmd('display_power').stdout.strip().split('=')
+        if display_status[1] == '1':
+            return True
+        else:
+            return False
+    except:
+        return 'Unable to determine display power.'
 
 def get_network_interfaces():
     if_data = {}
@@ -152,6 +161,7 @@ def compile_report():
     report['uptime'] = get_uptime()
     report['kernel_modules'] = get_kernel_modules()
     report['monitor'] = get_monitor_status()
+    report['display_status'] = get_display_status()
     report['ifconfig'] = get_network_interfaces()
     report['hostname'] = socket.gethostname()
     report['playlist'] = get_playlist()
