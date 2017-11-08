557409e6f1021189a008e7fc68bb49270d008e56
antonmolodykh <antonmolodykh96@gmail.com>
2017-11-08 17:35:14 +0600

    Modernize assets endpoint

--- server.py
+++ server.py
@@ -158,7 +158,13 @@ def prepare_asset(request):
     req = Request(request.environ)
     data = None
 
-    data = json.loads(req.form['model'])
+    # For backward compatibility
+    try:
+        data = json.loads(req.data)
+    except ValueError:
+        data = json.loads(req.form['model'])
+    except TypeError:
+        data = json.loads(req.form['model'])
 
     def get(key):
         val = data.get(key, '')
@@ -391,6 +397,132 @@ class Asset(Resource):
             return '', 204  # return an OK with no content
 
 
+class AssetsNewVersion(Resource):
+    method_decorators = [auth_basic]
+
+    @swagger.doc({
+        'responses': {
+            '200': {
+                'description': 'List of assets',
+                'schema': {
+                    'type': 'array',
+                    'items': AssetModel
+
+                }
+            }
+        }
+    })
+    def get(self):
+        with db.conn(settings['database']) as conn:
+            assets = assets_helper.read(conn)
+            return assets
+
+    @api_response
+    @swagger.doc({
+        'parameters': [
+            {
+                'in': 'body',
+                'name': 'model',
+                'description': 'Adds a asset',
+                'schema': AssetModel,
+                'required': True
+            }
+        ],
+        'responses': {
+            '201': {
+                'description': 'Asset created',
+                'schema': AssetModel
+            }
+        }
+    })
+    def post(self):
+        asset = prepare_asset(request)
+        if url_fails(asset['uri']):
+            raise Exception("Could not retrieve file. Check the asset URL.")
+        with db.conn(settings['database']) as conn:
+            return assets_helper.create(conn, asset), 201
+
+
+class AssetNewVersion(Resource):
+    method_decorators = [api_response, auth_basic]
+
+    @swagger.doc({
+        'parameters': [
+            {
+                'name': 'asset_id',
+                'type': 'string',
+                'in': 'path',
+                'description': 'id of an asset'
+            }
+        ],
+        'responses': {
+            '200': {
+                'description': 'Asset',
+                'schema': AssetModel
+            }
+        }
+    })
+    def get(self, asset_id):
+        with db.conn(settings['database']) as conn:
+            return assets_helper.read(conn, asset_id)
+
+    @swagger.doc({
+        'parameters': [
+            {
+                'name': 'asset_id',
+                'type': 'string',
+                'in': 'path',
+                'description': 'id of an asset',
+                'required': True
+            },
+            {
+                'in': 'body',
+                'name': 'model',
+                'description': 'Adds a asset',
+                'schema': AssetModel,
+                'required': True
+            }
+        ],
+        'responses': {
+            '200': {
+                'description': 'Asset updated',
+                'schema': AssetModel
+            }
+        }
+    })
+    def put(self, asset_id):
+        with db.conn(settings['database']) as conn:
+            return assets_helper.update(conn, asset_id, prepare_asset(request))
+
+    @swagger.doc({
+        'parameters': [
+            {
+                'name': 'asset_id',
+                'type': 'string',
+                'in': 'path',
+                'description': 'id of an asset',
+                'required': True
+
+            },
+        ],
+        'responses': {
+            '204': {
+                'description': 'Deleted'
+            }
+        }
+    })
+    def delete(self, asset_id):
+        with db.conn(settings['database']) as conn:
+            asset = assets_helper.read(conn, asset_id)
+            try:
+                if asset['uri'].startswith(settings['assetdir']):
+                    os.remove(asset['uri'])
+            except OSError:
+                pass
+            assets_helper.delete(conn, asset_id)
+            return '', 204  # return an OK with no content
+
+
 class FileAsset(Resource):
     method_decorators = [api_response, auth_basic]
 
@@ -535,6 +667,8 @@ class AssetsControl(Resource):
 
 api.add_resource(Assets, '/api/v1/assets')
 api.add_resource(Asset, '/api/v1/assets/<asset_id>')
+api.add_resource(AssetsNewVersion, '/api/v1.1/assets')
+api.add_resource(AssetNewVersion, '/api/v1.1/assets/<asset_id>')
 api.add_resource(FileAsset, '/api/v1/file_asset')
 api.add_resource(PlaylistOrder, '/api/v1/assets/order')
 api.add_resource(Backup, '/api/v1/backup')
