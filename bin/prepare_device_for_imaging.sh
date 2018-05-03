de534f15dcf0a65a4f9748f27fae1144c2416a7b
Viktor Petersson <vpetersson@wireload.net>
2018-05-03 15:10:56 +0100

    Fixes cmdline.txt patching.

--- bin/prepare_device_for_imaging.sh
+++ bin/prepare_device_for_imaging.sh
@@ -7,7 +7,7 @@ echo -e "\n# Lock down GPU memory usage\ngpu_mem_256=96\ngpu_mem_512=128\ngpu_me
 wget -O /etc/init.d/resize2fs_once https://github.com/RPi-Distro/pi-gen/raw/dev/stage2/01-sys-tweaks/files/resize2fs_once
 chmod +x /etc/init.d/resize2fs_once
 systemctl enable resize2fs_once
-echo -n "init=/usr/lib/raspi-config/init_resize.sh" >> /boot/cmdline.txt
+sed -i '$s/$/ init=\/usr\/lib\/raspi-config\/init_resize.sh/' /boot/cmdline.txt
 
 # Clean the apt cache
 apt-get clean
