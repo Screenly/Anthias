fe52d8c80640cb2dd778cc350e3fc1290be993d5
Rusko124 <pavel.lvl@yandex.ru>
2018-09-11 11:04:53 +0600

    make upload files like html

--- server.py
+++ server.py
@@ -819,8 +819,7 @@ class FileAsset(Resource):
         req = Request(request.environ)
         file_upload = req.files.get('file_upload')
         filename = file_upload.filename
-
-        if guess_type(filename)[0].split('/')[0] not in ['image', 'video']:
+        if guess_type(filename)[0].split('/')[0] not in ['image', 'video', 'text']:
             raise Exception("Invalid file type.")
 
         file_path = path.join(settings['assetdir'], filename) + ".tmp"
