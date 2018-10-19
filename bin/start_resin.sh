fea110d5600cdad6f22e5352cbf4eb7aefc20224
Rusko124 <pavel.lvl@yandex.ru>
2018-10-19 13:40:15 +0600

    resin.io support

--- bin/start_resin.sh
+++ bin/start_resin.sh
@@ -2,7 +2,6 @@
 
 mkdir -p \
     /data/.config \
-    /data/.config/uzbl \
     /data/.screenly \
     /data/screenly \
     /data/screenly_assets
@@ -10,7 +9,6 @@ mkdir -p \
 cp -n ansible/roles/screenly/files/screenly.conf /data/.screenly/screenly.conf
 cp -n ansible/roles/screenly/files/screenly.db /data/.screenly/screenly.db
 cp -n loading.png /data/screenly/loading.png
-cp -n ansible/roles/screenly/files/uzbl-config /data/.config/uzbl/config-screenly
 
 if [ -n "${OVERWRITE_CONFIG}" ]; then
     echo "Requested to overwrite Screenly config file."
@@ -26,13 +24,76 @@ if [ -n "${MANAGEMENT_USER+x}" ] && [ -n "${MANAGEMENT_PASSWORD+x}" ]; then
     sed -i -e "s/^user=.*/user=${MANAGEMENT_USER}/" -e "s/^password=.*/password=${MANAGEMENT_PASSWORD}/" /data/.screenly/screenly.conf
 fi
 
-sed -i "/\[Service\]/ a\Environment=RESIN_UUID=${RESIN_DEVICE_UUID}" /etc/systemd/system/screenly-web.service
+cat << EOF > /etc/systemd/system/screenly-server.service
+[Unit]
+Description=Screenly Web UI
+After=network-online.target
 
-systemctl start X.service
-systemctl start matchbox.service
+[Service]
+WorkingDirectory=/home/pi/screenly
+User=pi
+
+Environment=HOME=/data
+Environment=PYTHONPATH=/home/pi/screenly
+Environment=LISTEN=0.0.0.0
+Environment=SWAGGER_HOST=ose.demo.screenlyapp.com
+Environment=RESIN_UUID=${RESIN_DEVICE_UUID}
+
+ExecStartPre=/usr/bin/python /home/pi/screenly/bin/wait.py
+ExecStart=/usr/bin/python /home/pi/screenly/server.py
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > /etc/systemd/system/screenly-viewer.service
+[Unit]
+Description=Screenly Viewer
+After=screenly-server.service
+
+[Service]
+WorkingDirectory=/home/pi/screenly
+User=pi
+
+Environment=HOME=/data
+Environment=QT_QPA_EGLFS_FORCE888=1
+Environment=QT_QPA_EGLFS_PHYSICAL_WIDTH=154
+Environment=QT_QPA_EGLFS_PHYSICAL_HEIGHT=86
+Environment=QT_QPA_EGLFS_WIDTH=1280
+Environment=QT_QPA_EGLFS_HEIGHT=1024
+Environment=QT_QPA_PLATFORM=eglfs
+Environment=DISABLE_UPDATE_CHECK=True
+
+ExecStart=/usr/bin/dbus-run-session /usr/bin/python /home/pi/screenly/viewer.py
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > /etc/systemd/system/screenly-websocket.service
+[Unit]
+Description=Websocket Server layer
+After=screenly-server.service
+
+[Service]
+WorkingDirectory=/home/pi/screenly
+User=pi
+
+Environment=HOME=/data
+
+ExecStart=/usr/bin/python /home/pi/screenly/websocket_server_layer.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl start screenly-server.service
 systemctl start screenly-viewer.service
-systemctl start screenly-web.service
-systemctl start screenly-websocket_server_layer.service
+systemctl start screenly-websocket.service
 
 # By default docker gives us 64MB of shared memory size but to display heavy
 # pages we need more.
