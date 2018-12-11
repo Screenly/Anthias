57933319e3a273791034983408fc5eb725da83aa
Rusko124 <pavel.lvl@yandex.ru>
2018-12-11 15:05:27 +0600

    edits

--- ansible/roles/screenly/files/upgrade_screenly.sh
+++ ansible/roles/screenly/files/upgrade_screenly.sh
@@ -67,7 +67,7 @@ curl -s https://bootstrap.pypa.io/get-pip.py | sudo python
 
 sudo pip install ansible==2.7.1
 
-#ansible localhost -m git -a "repo=${1:-https://github.com/screenly/screenly-ose.git} dest=/home/pi/screenly version=$BRANCH"
+ansible localhost -m git -a "repo=${1:-https://github.com/screenly/screenly-ose.git} dest=/home/pi/screenly version=$BRANCH"
 cd /home/pi/screenly/ansible
 
 ansible-playbook site.yml $EXTRA_ARGS
