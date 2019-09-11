311790eb272c123ba239dbdeff43dacad31e4f7a
Rusko124 <pavel.lvl@yandex.ru>
2019-09-11 14:00:54 +0600

    Small code quality fix for system_info

--- server.py
+++ server.py
@@ -1676,12 +1676,11 @@ def settings_page():
 @app.route('/system-info')
 @authorized
 def system_info():
-    viewlog = None
     try:
         viewlog = [line.decode('utf-8') for line in
                    check_output(['sudo', 'systemctl', 'status', 'screenly-viewer.service', '-n', '20']).split('\n')]
     except Exception:
-        pass
+        viewlog = None
 
     loadavg = diagnostics.get_load_avg()['15 min']
 
