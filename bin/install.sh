7435680ffdebb975d10dd1ded0c43a660983a68f
Emyll Almonte <eg0dz@aol.com>
2020-03-04 17:01:29 -0500

    Update install.sh
    Many users report not knowing how to get a specific version of Screenly, thus changing the installer to show all three options seems like a better way to offer the different installations supported.

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# clear the screen
+clear;
+
 WEB_UPGRADE=false
 BRANCH_VERSION=
 MANAGE_NETWORK=
@@ -30,9 +33,9 @@ if [ "$WEB_UPGRADE" = false ]; then
     exit 1
   fi
 
-  # Set color of logo
-  tput setaf 4
-
+  # Set color and bold font of logo
+  tput setaf 6
+  tput bold
   cat << EOF
        _____                           __         ____  _____ ______
       / ___/_____________  ___  ____  / /_  __   / __ \/ ___// ____/
@@ -42,7 +45,7 @@ if [ "$WEB_UPGRADE" = false ]; then
                                      /____/
 EOF
 
-  # Reset color
+  # Reset color and bold font
   tput sgr 0
 
   echo -e "Screenly OSE requires a dedicated Raspberry Pi / SD card.\nYou will not be able to use the regular desktop environment once installed.\n"
@@ -52,20 +55,24 @@ EOF
     exit 1
   fi
 
-  echo && read -p "Would you like to use the experimental branch? It contains the last major changes, such as the new browser and migrating to Docker (y/N)" -n 1 -r -s EXP && echo
-  if [ "$EXP" != 'y'  ]; then
-    echo && read -p "Would you like to use the development (master) branch? You will get the latest features, but things may break. (y/N)" -n 1 -r -s DEV && echo
-    if [ "$DEV" != 'y'  ]; then
-      export DOCKER_TAG="production"
-      BRANCH="production"
-    else
-      export DOCKER_TAG="latest"
-      BRANCH="master"
-    fi
-  else
-    export DOCKER_TAG="experimental"
-    BRANCH="experimental"
-  fi
+echo -e "\n___________________________________________\n"
+echo -e "Which version/branch of Screenly-OSE would you like to install:\n"
+echo " Press (1) for the Production branch, which is the latest stable."
+echo " Press (2) for the Development/Master branch, which has the latest features and fixes, but things may break."
+echo " Press (3) for the Experimental branch, which contains the last major changes, such as the new browser and migrating to Docker."
+echo ""
+
+read -n 1 -r -s BRANCHSELECTION
+case $BRANCHSELECTION in
+  1) echo "You selected: Production";export DOCKER_TAG="production";BRANCH="production"
+    ;;
+  2) echo "You selected: Development/Master";export DOCKER_TAG="latest";BRANCH="master"
+    ;;
+  3) echo "You selected: Experimental";export DOCKER_TAG="experimental";BRANCH="experimental"
+    ;;
+  *) echo "(Error) That was not an option, installer will now exit.";exit
+    ;;
+esac
 
   echo && read -p "Would you like to install the WoTT agent to help you manage security of your Raspberry Pi? (y/N)" -n 1 -r -s WOTT && echo
   if [ "$WOTT" = 'y' ]; then
@@ -156,7 +163,8 @@ else
   export MANAGE_NETWORK=false
 fi
 
-sudo pip install ansible==2.8.2
+# playbook requires ansible
+sudo pip install ansible
 
 sudo -u pi ansible localhost -m git -a "repo=$REPOSITORY dest=/home/pi/screenly version=$BRANCH"
 cd /home/pi/screenly/ansible
