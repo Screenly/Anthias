a35e58ec73270201219938902c5b94009736e76c
Viktor Petersson <vpetersson@screenly.io>
2020-11-19 10:26:11 +0000

    Fixes build pushes

--- bin/build_containers.sh
+++ bin/build_containers.sh
@@ -22,13 +22,24 @@ for container in server celery redis websocket; do
     docker "${DOCKER_BUILD_ARGS[@]}" \
         -f "docker/Dockerfile.$container" \
         -t "screenly/srly-ose-$container:$GITBRANCH" .
+
+    # Push if the push flag is set and not cross compiling
+    if [[ ( -n "${PUSH+x}" && -z "${CROSS_COMPILE+x}" ) ]]; then
+        docker push "screenly/srly-ose-$container:$GITBRANCH" .
+    fi
 done
 
 echo "Building viewer for different architectures..."
 for pi_version in pi1 pi2 pi3; do
+    echo "Building viewer container for $pi_version"
     docker "${DOCKER_BUILD_ARGS[@]}" \
         --build-arg "PI_VERSION=$pi_version" \
         -f docker/Dockerfile.viewer \
         -t "screenly/srly-ose-viewer:$GITBRANCH-$pi_version" .
+
+    # Push if the push flag is set and not cross compiling
+    if [[ ( -n "${PUSH+x}" && -z "${CROSS_COMPILE+x}" ) ]]; then
+        docker push -t "screenly/srly-ose-viewer:$GITBRANCH-$pi_version"
+    fi
 done
 
