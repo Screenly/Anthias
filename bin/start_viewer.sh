69fb90e6f8b6707a865f49a6e60fba9106a7372c
Viktor Petersson <vpetersson@screenly.io>
2021-01-22 11:51:20 +0000

    Silence chown output

--- bin/start_viewer.sh
+++ bin/start_viewer.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # Fixes permission on /dev/vchiq
-chgrp video /dev/vchiq
-chmod g+rwX /dev/vchiq
+chgrp -f video /dev/vchiq
+chmod -f g+rwX /dev/vchiq
 
 # Set permission for sha file
-chown viewer /dev/snd/*
-chown viewer /data/.screenly/latest_screenly_sha
+chown -f viewer /dev/snd/*
+chown -f viewer /data/.screenly/latest_screenly_sha
 
 # Fixes caching in QTWebEngine
 mkdir -p /data/.local/share/ScreenlyWebview/QtWebEngine \
     /data/.cache/ScreenlyWebview \
     /data/.pki
-chown -R viewer /data/.local/share/ScreenlyWebview
-chown -R viewer /data/.cache/ScreenlyWebview/
-chown -R viewer /data/.pki
+chown -Rf viewer /data/.local/share/ScreenlyWebview
+chown -Rf viewer /data/.cache/ScreenlyWebview/
+chown -Rf viewer /data/.pki
 
 # Temporary workaround for watchdog
 touch /tmp/screenly.watchdog
 chown viewer /tmp/screenly.watchdog
 
 # For whatever reason Raspbian messes up the sudo permissions
-chown root:root /usr/bin/sudo
-chown -R root:root /etc/sudoers.d
-chown -R root:root /etc/sudo.conf
-chown -R root:root /usr/lib/sudo
-chown root:root /etc/sudoers
-chmod 4755 /usr/bin/sudo
+chown -f root:root /usr/bin/sudo
+chown -Rf root:root /etc/sudoers.d
+chown -Rf root:root /etc/sudo.conf
+chown -Rf root:root /usr/lib/sudo
+chown -f root:root /etc/sudoers
+chmod -f 4755 /usr/bin/sudo
 
 # SUGUSR1 from the viewer is also sent to the container
 # Prevent it so that the container does not fail
