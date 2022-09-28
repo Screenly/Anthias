37ce5faa80aa51cc1f5d9d71d34a67c243ab5f8e
nicomiguelino <nicomiguelino2014@gmail.com>
2022-09-28 11:55:24 -0700

    Add support for running tests locally (via docker-compose).
    - Add initial changes (for running tests) to docker-build.yaml as well.

---
+++ bin/run_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+prepare () {
+    mkdir -p ~/.screenly ~/.config/uzbl/ ~/screenly_assets /tmp/USB/cleanup_folder
+    curl https://www.screenly.io/upload/ose-logo.png > /tmp/image.png
+    cp /tmp/image.png /tmp/USB/image.png
+    cp /tmp/image.png /tmp/USB/cleanup_folder/image.png
+    cp tests/config/ffserver.conf /etc/ffserver.conf
+    python server.py &
+    server_pid=$!
+    sleep 3
+}
+
+execute_tests () {
+    find . ! -path "*/rtmplite/*" -name \*.py -exec pep8 --ignore=E402,E501,E731 {} +
+    nosetests -v -a '!fixme'
+}
+
+prepare
+execute_tests
+
+if [ -n "$server_pid" ]; then
+    kill $server_pid
+fi
