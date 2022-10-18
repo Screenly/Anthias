ecf3404488c015c1ec5aff119840958296d1d697
nicomiguelino <nicomiguelino2014@gmail.com>
2022-10-17 23:07:33 -0700

    Move the process of building ffmpeg binaries into Dockerfile instead.

--- bin/prepare_test_environment.sh
+++ bin/prepare_test_environment.sh
@@ -6,8 +6,6 @@
 set -euo pipefail
 
 START_SERVER=false
-# TODO: You could just fetch from within the repo instead.
-OSE_LOGO_URL='https://github.com/Screenly/screenly-ose/raw/master/static/img/ose-logo.png'
 
 print_usage () {
     echo "Usage: $0 [-s]"
@@ -39,17 +37,8 @@ cp /tmp/image.png /tmp/USB/image.png
 cp /tmp/image.png /tmp/USB/cleanup_folder/image.png
 cp tests/config/ffserver.conf /etc/ffserver.conf
 
-if [ ! -d /data/ffmpeg ] || [ ! -f /data/ffmpeg/ffserver ]; then
-    cd /data
-    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-    cd ffmpeg
-    git checkout 2ca65fc7b74444edd51d5803a2c1e05a801a6023
-    ./configure --disable-x86asm
-    make -j4
-fi
-
-nohup /data/ffmpeg/ffserver -f /etc/ffserver.conf > /dev/null 2>&1 &
-sleep 1
+nohup /opt/ffmpeg/ffserver -f /etc/ffserver.conf > /dev/null 2>&1 &
+sleep 3
 
 if [ "$START_SERVER" = true ]; then
     cd /usr/src/app
