b37f5ee71171b6df1f5b2ed0d0a4da9f36c88964
Emyll Almonte <eg0dz@aol.com>
2022-11-22 16:04:20 -0500

    Update screenly_usb_assets.sh

--- ansible/roles/screenly/files/screenly_usb_assets.sh
+++ ansible/roles/screenly/files/screenly_usb_assets.sh
@@ -3,11 +3,11 @@
 while :; do
     case $1 in
         add)
-            sudo -u pi /usr/local/bin/celery call --workdir /home/pi/screenly -A server.celery --args=[\"$2\"] server.append_usb_assets
+            sudo -u ${USER} /usr/local/bin/celery call --workdir /home/${USER}/screenly -A server.celery --args=[\"$2\"] server.append_usb_assets
             exit 0
             ;;
         remove)
-            sudo -u pi /usr/local/bin/celery call --workdir /home/pi/screenly -A server.celery --args=[\"$2\"] server.remove_usb_assets
+            sudo -u ${USER} /usr/local/bin/celery call --workdir /home/${USER}/screenly -A server.celery --args=[\"$2\"] server.remove_usb_assets
             exit 0
             ;;
         *)
