603a4d26fe246b3cd9d68cfc0fe7a916b29b46ec
Viktor Petersson <self@vpetersson.com>
2023-02-16 11:15:04 +0000

    Brings in build_containers.sh into CI pipeline

--- bin/build_containers.sh
+++ bin/build_containers.sh
@@ -8,6 +8,11 @@ set -euo pipefail
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GIT_SHORT_HASH=$(git rev-parse --short HEAD)
 GIT_HASH=$(git rev-parse HEAD)
+BASE_IMAGE_TAG=buster
+DEBIAN_VERSION=buster
+QT_VERSION=5.15.2
+WEBVIEW_GIT_HASH=0b6d49359133246659b9ba1d8dd883e3fc5c9a91
+WEBVIEW_BASE_URL="https://github.com/Screenly/Anthias/releases/download/WebView-v0.2.1"
 
 DOCKER_BUILD_ARGS=("buildx" "build" "--load")
 echo 'Make sure you ran `docker buildx create --use` before the command'
@@ -17,42 +22,34 @@ if [ -n "${CLEAN_BUILD+x}" ]; then
 fi
 
 # Set various variables for the Dockerfiles to use
-export BASE_IMAGE_TAG=buster
-export DEBIAN_VERSION=buster
-export QT_VERSION=5.15.2
-export GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
-export GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-export GIT_HASH="$(git rev-parse HEAD)"
-export WEBVIEW_GIT_HASH=0b6d49359133246659b9ba1d8dd883e3fc5c9a91
-export WEBVIEW_BASE_URL="https://github.com/Screenly/Anthias/releases/download/WebView-v0.2.1"
 
 # Detect what platform
-if [ ! -f /proc/device-tree/model ] && [ -n "${BUILD_TARGET+x}" ]; then
-    export BOARD="x86"
-    export BASE_IMAGE=debian
-    export TARGET_PLATFORM=linux/amd64
+if [ ! -f /proc/device-tree/model ] && [ -z "${BUILD_TARGET+x}" ]; then
+    BOARD="x86"
+    BASE_IMAGE=debian
+    TARGET_PLATFORM=linux/amd64
 elif grep -qF "Raspberry Pi 4" /proc/device-tree/model || [ "${BUILD_TARGET+x}" == 'pi4' ]; then
-    export BASE_IMAGE=balenalib/raspberrypi3-debian
-    export BOARD="pi4"
-    export TARGET_PLATFORM=linux/arm/v8
+    BASE_IMAGE=balenalib/raspberrypi3-debian
+    BOARD="pi4"
+    TARGET_PLATFORM=linux/arm/v8
 elif grep -qF "Raspberry Pi 3" /proc/device-tree/model || [ "${BUILD_TARGET+x}" == 'pi3' ]; then
-    export BOARD="pi3"
-    export BASE_IMAGE=balenalib/raspberrypi3-debian
-    export TARGET_PLATFORM=linux/arm/v7
+    BOARD="pi3"
+    BASE_IMAGE=balenalib/raspberrypi3-debian
+    TARGET_PLATFORM=linux/arm/v7
 elif grep -qF "Raspberry Pi 2" /proc/device-tree/model || [ "${BUILD_TARGET+x}" == 'pi2' ]; then
-    export BOARD="pi2"
-    export BASE_IMAGE=balenalib/raspberry-pi2
-    export TARGET_PLATFORM=linux/arm/v6
+    BOARD="pi2"
+    BASE_IMAGE=balenalib/raspberry-pi2
+    TARGET_PLATFORM=linux/arm/v6
 elif grep -qF "Raspberry Pi 1" /proc/device-tree/model || [ "${BUILD_TARGET+x}" == 'pi4' ]; then
-    export BOARD="pi1"
-    export BASE_IMAGE=balenalib/raspberry-pi
-    export TARGET_PLATFORM=linux/arm/v6
+    BOARD="pi1"
+    BASE_IMAGE=balenalib/raspberry-pi
+    TARGET_PLATFORM=linux/arm/v6
 fi
 
 if [ "$GIT_BRANCH" = "master" ]; then
-    export DOCKER_TAG="latest-$BOARD"
+    DOCKER_TAG="latest-$BOARD"
 else
-    export DOCKER_TAG="$GIT_BRANCH-$BOARD"
+    DOCKER_TAG="$GIT_BRANCH-$BOARD"
 fi
 
 
@@ -78,7 +75,7 @@ for container in base server celery redis websocket nginx viewer wifi-connect 't
         --cache-to "type=local,dest=/tmp/.buildx-cache" \
         --platform "$TARGET_PLATFORM" \
         -f "docker/Dockerfile.$container" \
-        -t "screenly/srly-ose-$container:latests" \
+        -t "screenly/srly-ose-$container:latest" \
         -t "screenly/srly-ose-$container:$DOCKER_TAG" .
 
     # Push if the push flag is set and not cross compiling
