618be07f994994af8297da4e16d09fb24f27d5d2
nicomiguelino <nicomiguelino2014@gmail.com>
2023-04-12 17:39:58 -0700

    Write unit test cases for generating and recovering backup files.

--- tests/backup_helper_test.py
+++ tests/backup_helper_test.py
@@ -1,16 +1,39 @@
 import mock
+import shutil
 import unittest
 from datetime import datetime
-from lib.backup_helper import create_backup
+from os import getenv, path
+from lib.backup_helper import create_backup, recover, static_dir
 
-dt = datetime(2016, 7, 19, 12, 42, 12)
+home = getenv('HOME')
+
 
 class BackupHelperTest(unittest.TestCase):
     def setUp(self):
-        # @TODO: Delete all archive files in ~/screenly/static/*.tar.gz.
-        shutil.rmtree('/home/pi/screenly/static/*.tar.gz', ignore_errors=True)
+        self.dt = datetime(2016, 7, 19, 12, 42, 12)
+        self.expected_archive_name = 'screenly-backup-2016-07-19T12-42-12.tar.gz'
+        self.assertFalse(path.isdir(path.join(home, static_dir)))
+
+    def tearDown(self):
+        shutil.rmtree(
+            path.join(home, 'screenly'),
+            ignore_errors=True,
+        )
+
+    def get_patched_datetime(self):
+        return mock.patch('lib.backup_helper.datetime')
+
     def test_get_backup_name(self):
-        with mock.patch('lib.backup_helper.datetime') as mock_datetime:
-            mock_datetime.now.return_value = dt
+        with self.get_patched_datetime() as mock_datetime:
+            mock_datetime.now.return_value = self.dt
             archive_name = create_backup()
-            self.assertEqual(archive_name, 'screenly-backup-2016-07-19T12-42-12.tar.gz')
+            self.assertEqual(archive_name, self.expected_archive_name)
+
+    def test_recover(self):
+        # TODO: Make the tests more specific.
+        #    For example, we can check if the individual files are present.
+        archive_name = create_backup()
+        file_path = path.join(home, static_dir, archive_name)
+        self.assertTrue(path.isfile(file_path))
+        recover(file_path)
+        self.assertFalse(path.isfile(file_path))
