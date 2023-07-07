39dbb8c6150367474d2ba495e5d0a527c94fd3ea
nicomiguelino <nicomiguelino2014@gmail.com>
2023-07-06 21:25:10 -0700

    Complete implementation for the assets list endpoint.

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import (
     CharField,
+    DateTimeField,
+    IntegerField,
     Serializer,
 )
 
 class AssetListSerializer(Serializer):
-    # @TODO: Remove these dummy fields.
-    field1 = CharField()
-    field2 = CharField()
+    asset_id = CharField()
+    name = CharField()
+    uri = CharField()
+    start_date = DateTimeField()
+    end_date = DateTimeField()
+    duration = CharField()
+    mimetype = CharField()
+    is_active = IntegerField(min_value=0, max_value=1)
+    is_enabled = IntegerField(min_value=0, max_value=1)
+    is_processing = IntegerField(min_value=0, max_value=1)
+    nocache = IntegerField()
+    play_order = IntegerField()
+    skip_asset_check = IntegerField(min_value=0, max_value=1)
--- api/views/v1.py
+++ api/views/v1.py
@@ -1,18 +1,23 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from api.serializers import AssetListSerializer
+from lib import (
+    assets_helper,
+    db,
+)
+from settings import settings
 
 
 class AssetListViewV1(APIView):
     serializer_class = AssetListSerializer
 
     def get(self, request, format=None):
-        data = {
-            'field1': 'value1',
-            'field2': 'value2',
-        }
-        serializer = self.serializer_class(data=data)
+        with db.conn(settings['database']) as conn:
+            data = assets_helper.read(conn)
+
+        serializer = self.serializer_class(data=data, many=True)
 
         if serializer.is_valid():
             return Response(serializer.data)
