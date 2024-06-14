6c90a74f1edb9d2f2dedb26868267dbd9b0633e1
nicomiguelino <nicomiguelino2014@gmail.com>
2024-06-14 16:21:59 -0700

    fix: resurrect the Screenly migration script
    * only tested and working on Raspberry Pi OS installation

--- tools/assets-migration-to-screenly.py
+++ tools/assets-migration-to-screenly.py
@@ -13,54 +13,19 @@ import time
 
 from requests.auth import HTTPBasicAuth
 
-user_home_dir = os.getenv('HOME')
+HOME = os.getenv('HOME')
 
 BASE_API_SCREENLY_URL = 'https://api.screenlyapp.com'
-ASSETS_SCREENLY_OSE_API = 'http://127.0.0.1/api/v1.1/assets'
+ASSETS_ANTHIAS_API = 'http://127.0.0.1/api/v1.1/assets'
 
-PORT_NGROK = 4040
 PORT = 80
 
 token = None
-ngrok_public_url = None
 
 
 ################################
 # Suprocesses
 ################################
-
-def start_http_ngrok_process(try_connection=100):
-    click.echo(click.style("Ngrok starting ...", fg='yellow'))
-
-    sh.ngrok('http', str(PORT), _bg=True, _in=os.devnull, _out=os.devnull, _err=sys.stderr)
-
-    try_count = 0
-    while True:
-        if try_count >= try_connection:
-            raise Exception('Failed start ngrok')
-        try:
-            requests.get('http://127.0.0.1:%i' % PORT_NGROK, timeout=10)
-            break
-        except requests.exceptions.ConnectionError:
-            try_count += 1
-            time.sleep(0.1)
-
-    click.echo(click.style("Ngrok successfull started", fg='green'))
-
-
-def get_ngrock_public_url(try_connection=100):
-    try_count = 0
-    while True:
-        if try_count >= try_connection:
-            raise Exception('Could not take a public url ngrok')
-        response = requests.get('http://127.0.0.1:%i/api/tunnels' % PORT_NGROK, timeout=10).json()
-        if response['tunnels']:
-            break
-        else:
-            try_count += 1
-            time.sleep(0.1)
-            continue
-    return response['tunnels'][0]['public_url']
 
 
 ################################
@@ -72,7 +37,7 @@ def progress_bar(count, total, text=''):
     This simple console progress bar
     For display progress asset uploads
     """
-    progress_line = "\xe2" * int(round(50 * count / float(total))) + '-' * (50 - int(round(50 * count / float(total))))
+    progress_line = "\u2588" * int(round(50 * count / float(total))) + '-' * (50 - int(round(50 * count / float(total))))
     percent = round(100.0 * count / float(total), 1)
     sys.stdout.write('[%s] %s%s %s\r' % (progress_line, percent, '%', text))
     sys.stdout.flush()
@@ -83,23 +48,18 @@ def set_token(value):
     token = 'Token %s' % value
 
 
-def set_ngrok_public_url(value):
-    global ngrok_public_url
-    ngrok_public_url = value
-
-
 ################################
 # Database
 ################################
 
-def get_assets_by_screenly_ose_api():
-    if click.confirm('Do you need authentication to access Screenly-OSE API?'):
+def get_assets_by_anthias_api():
+    if click.confirm('Do you need authentication to access Anthias API?'):
         login = click.prompt('Login')
         password = click.prompt('Password', hide_input=True)
         auth = HTTPBasicAuth(login, password)
     else:
         auth = None
-    response = requests.get(ASSETS_SCREENLY_OSE_API, timeout=10, auth=auth)
+    response = requests.get(ASSETS_ANTHIAS_API, timeout=10, auth=auth)
     if response.status_code == 200:
         return response.json()
     elif response.status_code == 401:
@@ -111,27 +71,49 @@ def get_assets_by_screenly_ose_api():
 ################################
 
 def send_asset(asset):
-    endpoind_url = '%s/api/v3/assets/' % BASE_API_SCREENLY_URL
+    endpoint_url = '%s/api/v3/assets/' % BASE_API_SCREENLY_URL
     headers = {
         'Authorization': token
     }
     asset_uri = asset['uri']
-    if asset_uri.startswith(user_home_dir):
-        asset_uri = os.path.join(ngrok_public_url, asset['asset_id'])
+
+    if asset_uri.startswith('/data'):
+        asset_uri = os.path.join(HOME, 'screenly_assets', os.path.basename(asset_uri))
+
     data = {
         'title': asset['name'],
         'source_url': asset_uri
     }
-    response = requests.post(endpoind_url, data=data, headers=headers)
-    return response.status_code == 200
+
+    post_kwargs = {
+        'data': data,
+        'headers': headers,
+    }
+
+    if asset['mimetype'] in ['image', 'video']:
+        post_kwargs.update({
+            'files': {
+                'file': open(asset_uri, 'rb') # @TODO: Add exception handling.
+            }
+        })
+
+    response = requests.post(endpoint_url, **post_kwargs)
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        click.echo(click.style('Error: %s' % e, fg='red'))
+        return False
+
+    return True
 
 
 def check_validate_token(api_key):
-    endpoind_url = '%s/api/v3/assets/' % BASE_API_SCREENLY_URL
+    endpoint_url = '%s/api/v3/assets/' % BASE_API_SCREENLY_URL
     headers = {
         'Authorization': 'Token %s' % api_key
     }
-    response = requests.get(endpoind_url, headers=headers)
+    response = requests.get(endpoint_url, headers=headers)
     if response.status_code == 200:
         return api_key
     else:
@@ -139,12 +121,12 @@ def check_validate_token(api_key):
 
 
 def get_api_key_by_credentials(username, password):
-    endpoind_url = '%s/api/v3/tokens/' % BASE_API_SCREENLY_URL
+    endpoint_url = '%s/api/v3/tokens/' % BASE_API_SCREENLY_URL
     data = {
         'username': username,
         'password': password
     }
-    response = requests.post(endpoind_url, data=data)
+    response = requests.post(endpoint_url, data=data)
     if response.status_code == 200:
         return response.json()['token']
     else:
@@ -156,20 +138,18 @@ def get_api_key_by_credentials(username, password):
 
 def start_migration():
     if click.confirm('Do you want to start assets migration?'):
-        click.echo('\n')
-        start_http_ngrok_process()
-        set_ngrok_public_url(get_ngrock_public_url())
-        click.echo('\n')
         assets_migration()
 
 
 def assets_migration():
-    assets = get_assets_by_screenly_ose_api()
+    assets = get_assets_by_anthias_api()
     assets_length = len(assets)
+
     click.echo('\n')
     for index, asset in enumerate(assets):
         asset_name = str(asset['name'])
         progress_bar(index + 1, assets_length, text='Asset in migration progress: %s' % asset_name)
+
         status = send_asset(asset)
         if not status:
             click.echo(click.style('\n%s asset was failed migration' % asset_name, fg='red'))
@@ -179,8 +159,8 @@ def assets_migration():
 
 @click.command()
 @click.option('--method',
-              prompt='What do you want to use for migration?\n1.API token\n2.Credentials\n0.Exit\nYour choice',
-              type=click.Choice(['1', '2', '0']))
+              prompt='What do you want to use for migration?\n1.API token\n2.Credentials\n3.Exit\nYour choice',
+              type=click.Choice(['1', '2', '3']))
 def main(method):
     try:
         valid_token = None
@@ -192,7 +172,7 @@ def main(method):
             username = click.prompt('Your username')
             password = click.prompt('Your password', hide_input=True)
             valid_token = get_api_key_by_credentials(username, password)
-        elif method == '0':
+        elif method == '3':
             sys.exit(0)
 
         if valid_token:
@@ -207,12 +187,15 @@ def main(method):
 
 if __name__ == '__main__':
     click.echo(click.style("""
-       _____                           __         ____  _____ ______
-      / ___/_____________  ___  ____  / /_  __   / __ \/ ___// ____/
-      \__ \/ ___/ ___/ _ \/ _ \/ __ \/ / / / /  / / / /\__ \/ __/
-     ___/ / /__/ /  /  __/  __/ / / / / /_/ /  / /_/ /___/ / /___
-    /____/\___/_/   \___/\___/_/ /_/_/\__, /   \____//____/_____/
-                                     /____/
-    """, fg='blue'))
+           d8888            888     888
+          d88888            888     888       888
+         d88P888            888     888
+        d88P 888  88888b.   888888  88888b.   888   8888b.   .d8888b
+       d88P  888  888 '88b  888     888 '88b  888      '88b  88K
+      d88P   888  888  888  888     888  888  888  .d888888  'Y8888b.
+     d8888888888  888  888  Y88b.   888  888  888  888  888       X88
+    d88P     888  888  888   Y888   888  888  888  'Y888888   88888P'
+    ==================================================================
+    """, fg='cyan'))
 
     main()
