11fcfb4eaa1c3f9030c1d4584f531af7d6404833
nicomiguelino <nicomiguelino2014@gmail.com>
2024-07-03 15:24:12 -0700

    chore: revert changes to volume names for Anthias running in Pi OS (Lite)

--- bin/upgrade_containers.sh
+++ bin/upgrade_containers.sh
@@ -51,16 +51,3 @@ fi
 sudo -E docker compose \
     -f /home/${USER}/screenly/docker-compose.yml \
     up -d
-
-# Check if all the containers are running
-# If yes, remove screenly_resin-data, only if it's dangling.
-
-RUNNING_ANTHIAS_CONTAINERS=$(docker ps -q --filter "name=anthias")
-DANGLING_VOLUMES=$(docker volume ls -q --filter "dangling=true")
-
-if [ -n "$RUNNING_ANTHIAS_CONTAINERS" ]; then
-    if [ -n "$DANGLING_VOLUMES" ]; then
-        docker volume rm screenly_resin-data
-    fi
-fi
-
