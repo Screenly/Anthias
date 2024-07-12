bde4e8a81da18501da94e983bf7d72d9beb2f3c5
nicomiguelino <nicomiguelino2014@gmail.com>
2024-07-12 16:02:24 -0700

    fix: only copy the necessary NGINX files at build time

--- bin/build_containers.sh
+++ bin/build_containers.sh
@@ -11,6 +11,7 @@ export GIT_SHORT_HASH=$(git rev-parse --short HEAD)
 export GIT_HASH=$(git rev-parse HEAD)
 export BASE_IMAGE_TAG=buster
 export DEBIAN_VERSION=buster
+export ENVIRONMENT=${ENVIRONMENT:-production}
 
 declare -a SERVICES=(
     server
