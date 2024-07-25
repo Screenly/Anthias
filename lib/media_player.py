33d554225b5d520cd60099c9be8baabb3654a6f4
nicomiguelino <nicomiguelino2014@gmail.com>
2024-07-25 13:37:54 -0700

    chore: cleanup unused OMX code

--- lib/media_player.py
+++ lib/media_player.py
@@ -65,38 +65,3 @@ class VLCMediaPlayer(MediaPlayer):
 
     def is_playing(self):
         return self.player.get_state() in [vlc.State.Playing, vlc.State.Buffering, vlc.State.Opening]
-
-
-class OMXMediaPlayer(MediaPlayer):
-    def __init__(self):
-        MediaPlayer.__init__(self)
-        self._arch = machine()
-
-        self._run = None
-        self._player_args = list()
-        self._player_kwargs = dict()
-
-    def set_asset(self, uri, duration):
-        settings.load()
-
-        if self._arch in ('armv6l', 'armv7l', 'aarch64'):
-            self._player_args = ['omxplayer', uri]
-            self._player_kwargs = {'o': settings['audio_output'], 'layer': 1, '_bg': True, '_ok_code': [0, 124, 143]}
-        else:
-            self._player_args = ['mplayer', uri, '-nosound']
-            self._player_kwargs = {'_bg': True, '_ok_code': [0, 124]}
-
-        if duration and duration != 'N/A':
-            self._player_args = ['timeout', VIDEO_TIMEOUT + int(duration.split('.')[0])] + self._player_args
-
-    def play(self):
-        self._run = sh.Command(self._player_args[0])(*self._player_args[1:], **self._player_kwargs)
-
-    def stop(self):
-        try:
-            sh.killall('omxplayer.bin', _ok_code=[1])
-        except OSError:
-            pass
-
-    def is_playing(self):
-        return bool(self._run.process.alive)
