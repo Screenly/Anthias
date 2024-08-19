7b6ef8652ab4306a83653d8041cf7cbc6adecfca
nicomiguelino <nicomiguelino2014@gmail.com>
2024-08-19 08:03:07 -0700

    docs: update documentation of prompts in `README.md`

--- bin/install.sh
+++ bin/install.sh
@@ -36,8 +36,10 @@ function install_charm_gum() {
     fi
 
     sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | \
+        sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" \
+        | sudo tee /etc/apt/sources.list.d/charm.list
     sudo apt -y update && sudo apt -y install gum
 }
 
