7200220b35bb85856a9c76b23e3786678c7aac9d
nicomiguelino <nicomiguelino2014@gmail.com>
2024-08-19 13:50:57 -0700

    feat: migrate the `assets` SQLite table to Django

--- anthias_app/models.py
+++ anthias_app/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models # noqa F401
 
-# Create your models here.
+
+def generate_asset_id():
+    return uuid.uuid4().hex
+
+
+class Asset(models.Model):
+    asset_id = models.TextField(primary_key=True, default=generate_asset_id, editable=False)
+    name = models.TextField(blank=True, null=True)
+    uri = models.TextField(blank=True, null=True)
+    md5 = models.TextField(blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    duration = models.TextField(blank=True, null=True)
+    mimetype = models.TextField(blank=True, null=True)
+    is_enabled = models.IntegerField(default=0)
+    is_processing = models.IntegerField(default=0)
+    nocache = models.IntegerField(default=0)
+    play_order = models.IntegerField(default=0)
+    skip_asset_check = models.IntegerField(default=0)
+
+    class Meta:
+        managed = False
+        db_table = 'assets'
--- anthias_django/settings.py
+++ anthias_django/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'anthias_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/.screenly/screenly.db',
     }
 }
 
