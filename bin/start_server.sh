05211705d5a55aa7cab16bb8cfe3a35049b83d6e
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-10 09:16:24 -0700

    chore: remove unused code from `start_server.sh`

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -11,17 +11,6 @@ cp -n /usr/src/app/ansible/roles/screenly/files/screenly.conf /data/.screenly/sc
 cp -n /usr/src/app/ansible/roles/screenly/files/default_assets.yml /data/.screenly/default_assets.yml
 cp -n /usr/src/app/ansible/roles/screenly/files/screenly.db /data/.screenly/screenly.db
 
-if [ -n "${OVERWRITE_CONFIG}" ]; then
-    echo "Requested to overwrite Anthias config file."
-    cp /usr/src/app/ansible/roles/screenly/files/screenly.conf "/data/.screenly/screenly.conf"
-fi
-
-# Set management page's user and password from environment variables,
-# but only if both of them are provided. Can have empty values provided.
-if [ -n "${MANAGEMENT_USER+x}" ] && [ -n "${MANAGEMENT_PASSWORD+x}" ]; then
-    sed -i -e "s/^user=.*/user=${MANAGEMENT_USER}/" -e "s/^password=.*/password=${MANAGEMENT_PASSWORD}/" /data/.screenly/screenly.conf
-fi
-
 echo "Running migration..."
 python ./bin/migrate.py
 
