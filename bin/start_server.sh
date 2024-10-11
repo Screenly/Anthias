9bfca9fe71c45f7d061cd1802e64ea55ea3b1124
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-11 08:27:59 -0700

    fix: resolve code review comments
    * bring back the line that copies `screenly.db` from the `./ansible`
      directory
    * make a backup of `screenly.db` named `screenly.db.back`

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -9,14 +9,17 @@ mkdir -p \
 
 cp -n /usr/src/app/ansible/roles/screenly/files/screenly.conf /data/.screenly/screenly.conf
 cp -n /usr/src/app/ansible/roles/screenly/files/default_assets.yml /data/.screenly/default_assets.yml
+cp -n /usr/src/app/ansible/roles/screenly/files/screenly.db /data/.screenly/screenly.db
 
 echo "Running migration..."
 
 # The following block ensures that the migration is transactional and that the
 # database is not left in an inconsistent state if the migration fails.
+
 cp /data/.screenly/screenly.db /data/.screenly/backup.db
+cp /data/.screenly/screenly.db /data/.screenly/screenly.db.bak
 ./manage.py migrate --fake-initial --database=backup
-cp /data/.screenly/backup.db /data/.screenly/screenly.db
+mv /data/.screenly/backup.db /data/.screenly/screenly.db
 
 if [[ "$ENVIRONMENT" == "development" ]]; then
     echo "Starting Django development server..."
