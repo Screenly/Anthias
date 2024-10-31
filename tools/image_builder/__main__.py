c3d0b110475fb3f83feca911d98b8abe6efc1c51
nicomiguelino <nicomiguelino2014@gmail.com>
2024-10-31 13:41:41 -0700

    fix: add support for autogenerating `Dockerfile.viewer`

--- tools/image_builder/__main__.py
+++ tools/image_builder/__main__.py
@@ -21,6 +21,8 @@ SERVICES = (
 
 
 templating_environment = Environment(loader=FileSystemLoader('docker/'))
+templating_environment.lstrip_blocks = True
+templating_environment.trim_blocks = True
 
 
 def get_build_parameters(build_target: str) -> dict:
@@ -84,11 +86,133 @@ def build_image(
     service: str,
     board: str,
     target_platform: str,
+    disable_cache_mounts: bool,
 ) -> None:
+    context = {}
+
     if service == 'viewer':
         qt_version = '5.15.2'
         webview_git_hash='4bd295c4a1197a226d537938e947773f4911ca24'
         webview_base_url='https://github.com/Screenly/Anthias/releases/download/WebView-v0.3.1'
+
+        context.update({
+            'apt_dependencies': [
+                'build-essential',
+                'ca-certificates',
+                'curl',
+                'dbus-daemon',
+                'fonts-arphic-uming',
+                'git-core',
+                'libasound2-dev',
+                'libavcodec-dev',
+                'libavformat-dev',
+                'libavutil-dev',
+                'libbz2-dev',
+                'libcec-dev ',
+                'libdbus-1-dev',
+                'libdbus-glib-1-dev',
+                'libdrm-dev',
+                'libegl1-mesa-dev',
+                'libevent-dev',
+                'libffi-dev',
+                'libfontconfig1-dev',
+                'libfreetype6-dev',
+                'libgbm-dev',
+                'libgcrypt20-dev',
+                'libgles2-mesa',
+                'libgles2-mesa-dev',
+                'libglib2.0-dev',
+                'libgst-dev',
+                'libicu-dev',
+                'libinput-dev',
+                'libiodbc2-dev',
+                'libjpeg62-turbo-dev',
+                'libjsoncpp-dev',
+                'libminizip-dev',
+                'libnss3',
+                'libnss3-dev',
+                'libnss3-tools',
+                'libopus-dev',
+                'libpci-dev',
+                'libpng-dev',
+                'libpng16-16',
+                'libpq-dev',
+                'libpulse-dev',
+                'libraspberrypi0',
+                'librsvg2-common',
+                'libsdl2-dev',
+                'libsnappy-dev',
+                'libsqlite0-dev',
+                'libsqlite3-dev',
+                'libsrtp0-dev',
+                'libsrtp2-dev',
+                'libssl-dev',
+                'libzmq3-dev',
+                'libssl1.1',
+                'libswscale-dev',
+                'libsystemd-dev',
+                'libts-dev',
+                'libudev-dev',
+                'libvpx-dev',
+                'libwebp-dev',
+                'libx11-dev',
+                'libx11-xcb-dev',
+                'libx11-xcb1',
+                'libxcb-glx0-dev',
+                'libxcb-icccm4',
+                'libxcb-icccm4-dev',
+                'libxcb-image0',
+                'libxcb-image0-dev',
+                'libxcb-keysyms1',
+                'libxcb-keysyms1-dev',
+                'libxcb-randr0-dev',
+                'libxcb-render-util0',
+                'libxcb-render-util0-dev',
+                'libxcb-shape0-dev',
+                'libxcb-shm0',
+                'libxcb-shm0-dev',
+                'libxcb-sync-dev',
+                'libxcb-sync1',
+                'libxcb-xfixes0-dev',
+                'libxcb-xinerama0',
+                'libxcb-xinerama0-dev',
+                'libxcb1',
+                'libxcb1-dev',
+                'libxext-dev',
+                'libxi-dev',
+                'libxkbcommon-dev',
+                'libxrender-dev',
+                'libxslt1-dev',
+                'libxss-dev',
+                'libxtst-dev',
+                'libzmq5-dev',
+                'libzmq5',
+                'net-tools',
+                'procps',
+                'psmisc',
+                'python3-dev',
+                'python3-gi',
+                'python3-netifaces',
+                'python3-pip',
+                'python3-setuptools',
+                'python-is-python3',
+                'ttf-wqy-zenhei',
+                'vlc',
+                'sudo',
+                'sqlite3',
+                'ffmpeg',
+                'libavcodec-dev',
+                'libavdevice-dev',
+                'libavfilter-dev',
+                'libavformat-dev',
+                'libavutil-dev',
+                'libswresample-dev',
+                'libswscale-dev',
+            ],
+            'qt_version': qt_version,
+            'webview_git_hash': webview_git_hash,
+            'webview_base_url': webview_base_url,
+        })
     elif service == 'test':
         chrome_dl_url="https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chrome-linux64.zip"
         chromedriver_dl_url="https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip"
@@ -128,6 +252,8 @@ def build_image(
     generate_dockerfile(service, {
         'base_image': 'balenalib/raspberrypi3-debian',
         'base_image_tag': 'bookworm',
+        'disable_cache_mounts': disable_cache_mounts,
+        **context,
     })
 
 
@@ -150,10 +276,15 @@ def build_image(
     )),
     multiple=True,
 )
+@click.option(
+    '--disable-cache-mounts',
+    is_flag=True,
+)
 def main(
     clean_build: bool,
     build_target: str,
     service,
+    disable_cache_mounts: bool,
 ) -> None:
     git_branch = pygit2.Repository('.').head.shorthand
     git_hash = str(pygit2.Repository('.').head.target)
@@ -168,7 +299,7 @@ def main(
     services_to_build = SERVICES if 'all' in service else list(set(service))
 
     for service in services_to_build:
-        build_image(service, board, target_platform)
+        build_image(service, board, target_platform, disable_cache_mounts)
 
 
 if __name__ == "__main__":
