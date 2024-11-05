f778056254c2f2dc5b663d3f6380be3b3eea8a9d
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-05 10:26:01 -0800

    chore: remove remaining parts that still uses the old build script

--- bin/start_development_server.sh
+++ bin/start_development_server.sh
@@ -6,13 +6,29 @@ COMPOSE_ARGS=(
     '-f' 'docker-compose.dev.yml'
 )
 
-function generate_dockerfiles() {
-    ENVIRONMENT=development \
-    DOCKERFILES_ONLY=1 \
-    DISABLE_CACHE_MOUNTS=1 \
-    ./bin/build_containers.sh
+function initialize_python_environment() {
+    ./bin/install_poetry.sh
+
+    # Add `pyenv` to the load path.
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && \
+        export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    # Add `poetry to the load path.
+    export PATH="$HOME/.local/bin:$PATH"
+
+    poetry install --only=docker-image-builder
 }
 
+function generate_dockerfiles() {
+    poetry run python tools/image_builder \
+        --environment=development \
+        --dockerfiles-only \
+        --disable-cache-mounts
+}
+
+initialize_python_environment
 generate_dockerfiles
 docker compose "${COMPOSE_ARGS[@]}" down
 docker compose "${COMPOSE_ARGS[@]}" up -d --build
