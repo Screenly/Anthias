c0fb6a8db147059632a5a99d6f470ca5d7e58730
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-14 23:07:52 -0800

    chore(workflow): set up SASS pipeline in Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,53 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
-module.exports = {
-    devtool: "source-map",
-    mode: "development",
-    entry: {
-        "anthias": "./static/js/anthias.coffee",
-        "settings": "./static/js/settings.coffee",
+module.exports = [
+    {
+        devtool: "source-map",
+        mode: "development",
+        entry: {
+            "anthias": "./static/js/anthias.coffee",
+            "settings": "./static/js/settings.coffee",
+
+        },
+        output: {
+            path: path.resolve(__dirname, "static/js"),
+            filename: "[name].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.coffee$/,
+                    use: ["coffee-loader"]
+                },
+            ]
+        }
     },
-    output: {
-        path: path.resolve(__dirname, "static/js"),
-        filename: "[name].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.coffee$/,
-                use: ["coffee-loader"]
-            }
-        ]
-    },
-};
+    {
+        devtool: "source-map",
+        mode: "development",
+        entry: {
+            "anthias": "./static/sass/anthias.scss",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            })
+        ],
+        output: {
+            path: path.resolve(__dirname, "static/css"),
+        }
+    }
+];
