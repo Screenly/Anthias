b43c480b6cf3c99a89c7bf033ae334c7a942987a
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-14 16:47:05 -0800

    chore: resolve duplication in serializer code

--- api/serializers/v2.py
+++ api/serializers/v2.py
@@ -1,5 +1,3 @@
-import uuid
-from os import path, rename
 from django.utils import timezone
 from anthias_app.models import Asset
 from rest_framework.serializers import (
@@ -12,19 +10,10 @@ from rest_framework.serializers import (
 )
 
 from api.serializers import UpdateAssetSerializer
-from lib.utils import (
-    download_video_from_youtube,
-    get_video_duration,
-    url_fails,
-)
-from settings import settings
-from . import (
-    get_unique_name,
-    validate_uri,
-)
+from api.serializers.mixins import CreateAssetSerializerMixin
 
 
-class AssetSerializerV2(ModelSerializer):
+class AssetSerializerV2(ModelSerializer, CreateAssetSerializerMixin):
     class Meta:
         model = Asset
         fields = [
@@ -44,7 +33,7 @@ class AssetSerializerV2(ModelSerializer):
         ]
 
 
-class CreateAssetSerializerV2(Serializer):
+class CreateAssetSerializerV2(Serializer, CreateAssetSerializerMixin):
     def __init__(self, *args, unique_name=False, **kwargs):
         self.unique_name = unique_name
         super().__init__(*args, **kwargs)
@@ -63,79 +52,8 @@ class CreateAssetSerializerV2(Serializer):
     play_order = IntegerField(required=False)
     skip_asset_check = BooleanField(required=False)
 
-    def prepare_asset(self, data, asset_id=None):
-        ampersand_fix = '&amp;'
-        name = data['name'].replace(ampersand_fix, '&')
-
-        if self.unique_name:
-            name = get_unique_name(name)
-
-        asset = {
-            'name': name,
-            'mimetype': data.get('mimetype'),
-            'is_enabled': data.get('is_enabled', False),
-            'nocache': data.get('nocache', False),
-        }
-
-        uri = (
-            data['uri']
-            .replace(ampersand_fix, '&')
-            .replace('<', '&lt;')
-            .replace('>', '&gt;')
-            .replace('\'', '&apos;')
-            .replace('\"', '&quot;')
-        )
-
-        validate_uri(uri)
-
-        if not asset_id:
-            asset['asset_id'] = uuid.uuid4().hex
-
-        if not asset_id and uri.startswith('/'):
-            path_name = path.join(settings['assetdir'], asset['asset_id'])
-            ext_name = data.get('ext', '')
-            new_uri = f'{path_name}{ext_name}'
-            rename(uri, new_uri)
-            uri = new_uri
-
-        if 'youtube_asset' in asset['mimetype']:
-            (
-                uri, asset['name'], asset['duration']
-            ) = download_video_from_youtube(uri, asset['asset_id'])
-            asset['mimetype'] = 'video'
-            asset['is_processing'] = True
-
-        asset['uri'] = uri
-
-        if "video" in asset['mimetype']:
-            if data.get('duration') == 0:
-                asset['duration'] = int(
-                    get_video_duration(uri).total_seconds())
-        else:
-            # Crashes if it's not an int. We want that.
-            asset['duration'] = data.get(
-                'duration', settings['default_duration'])
-
-        asset['play_order'] = (
-            data.get('play_order') if data.get('play_order') else 0
-        )
-
-        asset['skip_asset_check'] = (
-            int(data.get('skip_asset_check'))
-            if int(data.get('skip_asset_check'))
-            else 0
-        )
-
-        asset['start_date'] = data.get('start_date').replace(tzinfo=None)
-        asset['end_date'] = data.get('end_date').replace(tzinfo=None)
-
-        if not asset['skip_asset_check'] and url_fails(asset['uri']):
-            raise Exception("Could not retrieve file. Check the asset URL.")
-
-        return asset
-
     def validate(self, data):
-        return self.prepare_asset(data)
+        return self.prepare_asset(data, version='v2')
 
 
 class UpdateAssetSerializerV2(UpdateAssetSerializer):
