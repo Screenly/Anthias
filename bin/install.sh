791079245ba3bb5029476c26fb2af6eecb65aaed
nicomiguelino <nicomiguelino2014@gmail.com>
2024-12-18 09:55:25 -0800

    fix: update the installer to be less instrusive for existing platforms

--- bin/install.sh
+++ bin/install.sh
@@ -181,8 +181,25 @@ function install_ansible() {
     sudo ${SUDO_ARGS[@]} pip install "$ANSIBLE_VERSION"
 }
 
+function set_device_type() {
+    if [ ! -f /proc/device-tree/model ] && [ "$(uname -m)" = "x86_64" ]; then
+        export DEVICE_TYPE="x86"
+    elif grep -qF "Raspberry Pi 5" /proc/device-tree/model; then
+        export DEVICE_TYPE="pi5"
+    elif grep -qF "Raspberry Pi 4" /proc/device-tree/model; then
+        export DEVICE_TYPE="pi4"
+    elif grep -qF "Raspberry Pi 3" /proc/device-tree/model; then
+        export DEVICE_TYPE="pi3"
+    elif grep -qF "Raspberry Pi 2" /proc/device-tree/model; then
+        export DEVICE_TYPE="pi2"
+    else
+        export DEVICE_TYPE="pi1"
+    fi
+}
+
 function run_ansible_playbook() {
     display_section "Run the Anthias Ansible Playbook"
+    set_device_type
 
     sudo -u ${USER} ${SUDO_ARGS[@]} ansible localhost \
         -m git \
