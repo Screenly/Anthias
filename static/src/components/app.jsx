e2b92bb5704dc0f20f1ad97f29c4c485a4ca023c
nicomiguelino <nicomiguelino2014@gmail.com>
2025-01-31 23:07:34 -0800

    fix: bring back the navbar, which was accidentally removed

--- static/src/components/app.jsx
+++ static/src/components/app.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import { ScheduleOverview } from './home';
+import { Routes, Route } from 'react-router'
+
+import { Integrations } from '@/components/integrations'
+import { Navbar } from '@/components/navbar'
+import { ScheduleOverview } from '@/components/home'
+import { Settings } from '@/components/settings'
+import { SystemInfo } from '@/components/system-info'
 
 export const App = () => {
   return (
-    <ScheduleOverview />
-  );
-};
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<ScheduleOverview />} />
+        <Route path="/integrations" element={<Integrations />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/system-info" element={<SystemInfo />} />
+      </Routes>
+    </>
+  )
+}
