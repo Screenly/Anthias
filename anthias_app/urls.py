fe3a379fd34045f29f10aae47c50250054f40b5b
nicomiguelino <nicomiguelino2014@gmail.com>
2025-04-27 13:16:36 -0700

    fix: restore the splash page

--- anthias_app/urls.py
+++ anthias_app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import re_path
+from django.urls import path, re_path # todo nico; remove path if not needed
 
 from . import views
 
 app_name = 'anthias_app'
 
 urlpatterns = [
+    path('splash-page', views.splash_page, name='splash_page'),
     re_path(r'^(?!api/).*$', views.react, name='react'),
 ]
