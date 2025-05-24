df44ba10b1125dec46f3390bab23c2f9d046a3bf
nicomiguelino <nicomiguelino2014@gmail.com>
2025-05-23 23:53:56 -0700

    fix: migrate the footer to React

--- static/src/components/app.jsx
+++ static/src/components/app.jsx
@@ -8,6 +8,7 @@ import { Navbar } from '@/components/navbar'
 import { ScheduleOverview } from '@/components/home'
 import { Settings } from '@/components/settings'
 import { SystemInfo } from '@/components/system-info'
+import { Footer } from '@/components/footer'
 
 export const App = () => {
   const dispatch = useDispatch()
@@ -26,6 +27,27 @@ export const App = () => {
         <Route path="/settings" element={<Settings />} />
         <Route path="/system-info" element={<SystemInfo />} />
       </Routes>
+
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-6 small text-white">
+            <span>
+              Want to get more out of your digital signage?
+              <a
+                className="brand"
+                href="https://www.screenly.io/?utm_source=Anthias&utm_medium=root-page&utm_campaign=UI"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {' '}
+                <strong>Try Screenly</strong>.
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
   )
 }
