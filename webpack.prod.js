aa720273a1df6a1b233ca4458cee9df892f8a784
Nico Miguelino <nicomiguelino2014@gmail.com>
2025-06-12 13:02:43 -0700

    Revert "chore: disable Redux DevTools in production"

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.ENVIRONMENT': JSON.stringify('production')
-    })
-  ]
 });
