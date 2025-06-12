e67f7daa8438a4f32c3397ea190debee0d60ae90
nicomiguelino <nicomiguelino2014@gmail.com>
2025-06-12 14:50:47 -0700

    chore: disable Redux DevTools in production

--- static/src/store/index.js
+++ static/src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { assetsReducer, assetModalReducer } from '@/store/assets'
 
+const environment = process.env.ENVIRONMENT || 'production'
+
 export const store = configureStore({
   reducer: {
     assets: assetsReducer,
     assetModal: assetModalReducer,
   },
+  devTools: environment === 'development',
 })
