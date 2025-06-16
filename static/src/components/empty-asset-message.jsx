a6431f6e1782250bc497b78baf72e4cccb274907
Nico Miguelino <nicomiguelino2014@gmail.com>
2025-06-16 14:14:52 -0700

    chore(ci): fix JavaScript formatting issues (#2344)

--- static/src/components/empty-asset-message.jsx
+++ static/src/components/empty-asset-message.jsx
@@ -1,12 +1,10 @@
 export const EmptyAssetMessage = ({ onAddAssetClick }) => {
   return (
     <div className="table-assets-help-text">
-      Currently, there are no assets.
-      {' '}
+      Currently, there are no assets.{' '}
       <a className="add-asset-button" href="#" onClick={onAddAssetClick}>
         Add asset
-      </a>
-      {' '}
+      </a>{' '}
       now.
     </div>
   )
