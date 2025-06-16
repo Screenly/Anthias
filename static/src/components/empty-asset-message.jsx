2961e10e45a9072592dd84a4a387aa364693092b
Nico Miguelino <nicomiguelino2014@gmail.com>
2025-06-16 12:51:07 -0700

    fix: add missing spaces and punctuations (#2342)

--- static/src/components/empty-asset-message.jsx
+++ static/src/components/empty-asset-message.jsx
@@ -1,10 +1,12 @@
 export const EmptyAssetMessage = ({ onAddAssetClick }) => {
   return (
     <div className="table-assets-help-text">
-      Currently there are no assets.
+      Currently, there are no assets.
+      {' '}
       <a className="add-asset-button" href="#" onClick={onAddAssetClick}>
         Add asset
-      </a>{' '}
+      </a>
+      {' '}
       now.
     </div>
   )
