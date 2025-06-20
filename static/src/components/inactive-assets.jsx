189a599c89d68ac757934e24118aee31df2ddb72
nicomiguelino <nicomiguelino2014@gmail.com>
2025-06-20 12:20:59 -0700

    chore: remove unnecessary React fragments

--- static/src/components/inactive-assets.jsx
+++ static/src/components/inactive-assets.jsx
@@ -6,66 +6,64 @@ export const InactiveAssetsTable = ({ onEditAsset }) => {
   const inactiveAssets = useSelector(selectInactiveAssets)
 
   return (
-    <>
-      <table className="InactiveAssets table">
-        <thead className="table-borderless">
-          <tr>
-            <th className="text-secondary font-weight-normal asset_row_name">
-              Name
-            </th>
-            <th
-              className="text-secondary font-weight-normal"
-              style={{ width: '21%' }}
-            >
-              Start
-            </th>
-            <th
-              className="text-secondary font-weight-normal"
-              style={{ width: '21%' }}
-            >
-              End
-            </th>
-            <th
-              className="text-secondary font-weight-normal"
-              style={{ width: '13%' }}
-            >
-              Duration
-            </th>
-            <th
-              className="text-secondary font-weight-normal"
-              style={{ width: '7%' }}
-            >
-              Activity
-            </th>
-            <th
-              className="text-secondary font-weight-normal"
-              style={{ width: '13%' }}
-            >
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody id="inactive-assets" className="table-borderless">
-          {inactiveAssets.map((asset) => (
-            <AssetRow
-              key={asset.asset_id}
-              name={asset.name}
-              startDate={asset.start_date}
-              endDate={asset.end_date}
-              duration={asset.duration}
-              isEnabled={asset.is_enabled}
-              assetId={asset.asset_id}
-              isProcessing={asset.is_processing}
-              uri={asset.uri}
-              mimetype={asset.mimetype}
-              nocache={asset.nocache}
-              skipAssetCheck={asset.skip_asset_check}
-              onEditAsset={onEditAsset}
-              showDragHandle={false}
-            />
-          ))}
-        </tbody>
-      </table>
-    </>
+    <table className="InactiveAssets table">
+      <thead className="table-borderless">
+        <tr>
+          <th className="text-secondary font-weight-normal asset_row_name">
+            Name
+          </th>
+          <th
+            className="text-secondary font-weight-normal"
+            style={{ width: '21%' }}
+          >
+            Start
+          </th>
+          <th
+            className="text-secondary font-weight-normal"
+            style={{ width: '21%' }}
+          >
+            End
+          </th>
+          <th
+            className="text-secondary font-weight-normal"
+            style={{ width: '13%' }}
+          >
+            Duration
+          </th>
+          <th
+            className="text-secondary font-weight-normal"
+            style={{ width: '7%' }}
+          >
+            Activity
+          </th>
+          <th
+            className="text-secondary font-weight-normal"
+            style={{ width: '13%' }}
+          >
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody id="inactive-assets" className="table-borderless">
+        {inactiveAssets.map((asset) => (
+          <AssetRow
+            key={asset.asset_id}
+            name={asset.name}
+            startDate={asset.start_date}
+            endDate={asset.end_date}
+            duration={asset.duration}
+            isEnabled={asset.is_enabled}
+            assetId={asset.asset_id}
+            isProcessing={asset.is_processing}
+            uri={asset.uri}
+            mimetype={asset.mimetype}
+            nocache={asset.nocache}
+            skipAssetCheck={asset.skip_asset_check}
+            onEditAsset={onEditAsset}
+            showDragHandle={false}
+          />
+        ))}
+      </tbody>
+    </table>
   )
 }
