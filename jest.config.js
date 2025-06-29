f72f086fd33fd7a791ec7a8ae8c08c2e47e49a58
nicomiguelino <nicomiguelino2014@gmail.com>
2025-06-29 00:17:22 -0700

    chore(ci): create a new pipeline for running TypeScript/React tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/static/src'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/static/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/static/src/setupTests.ts'],
+};
