5e77832b7cd343efde9acf837407c9d73095ddff
Nico Miguelino <nicomiguelino2014@gmail.com>
2025-07-03 18:47:41 -0700

    test: write unit tests for the System Info page (#2382)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-fixed-jsdom',
   roots: ['<rootDir>/static/src'],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   transform: {
