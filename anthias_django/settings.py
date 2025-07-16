ae61dc1f251077ce3bbb0c35421ec24ffb544f3d
nicomiguelino <nicomiguelino2014@gmail.com>
2025-07-16 07:59:32 -0700

    chore: add CORS handling in Django

--- anthias_django/settings.py
+++ anthias_django/settings.py
@@ -68,11 +68,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dbbackup',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
