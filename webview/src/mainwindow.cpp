b8a235ced63d204fa0f72821275ae83376d6dd2c
Nico Miguelino <nicomiguelino2014@gmail.com>
2025-07-31 13:44:34 -0700

    feat: explicitly enable full screen for x86 (#2411)

--- webview/src/mainwindow.cpp
+++ webview/src/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include <QStandardPaths>
 #include <QWebEngineSettings>
+#include <QGuiApplication>
+#include <QScreen>
 
 #include "mainwindow.h"
 #include "view.h"
@@ -11,6 +13,10 @@ MainWindow::MainWindow() : QMainWindow()
     view->webView->settings()->setAttribute(QWebEngineSettings::LocalStorageEnabled, true);
     view->webView->settings()->setAttribute(QWebEngineSettings::ShowScrollBars, false);
     setCentralWidget(view);
+
+    QRect screenGeometry = QGuiApplication::primaryScreen()->geometry();
+    setGeometry(screenGeometry);
+    showFullScreen();
 }
 
 void MainWindow::loadPage(const QString &uri)
